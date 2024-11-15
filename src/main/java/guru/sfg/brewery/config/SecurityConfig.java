dc309904013fcbea6095410d8189a3e37f0f1fe6
kamleshraul <kamleshraul@gmail.com>
2024-11-15 18:55:43 +0530

    InmemoryUserDetails created using UserDetails Api as well as Spring Security Fluent Api

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +33,50 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic(Customizer.withDefaults());
 			
 	}
+
+	// another way to create InMemoryUserDetails
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("spring")
+			.password("{noop}security") //{noop} is required as we not configuring any password encoder
+			.roles("ADMIN")
+			.and()
+			.withUser("user")
+			.password("{noop}password")
+			.roles("User")
+			.and()
+			.withUser("scott")
+			.password("{noop}tiger")
+			.roles("Customer");
+			
+	}
+
+	//creating our own inmemory userdetailservice for credentials
+	//as we implemented our own , user credentials defined in application.properties
+	//won't get used now
+	/*@Override
+	@Bean   
+	protected UserDetailsService userDetailsService() {
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("spring")
+				.password("security")
+				.roles("ADMIN")
+				.build();
+		
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(admin,user);
+		
+		
+	}
+	*/
+	
+	
 
 }
